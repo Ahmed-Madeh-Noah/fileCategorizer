6b6003af43ac8932c8bf8c90d8a8eaeb6c00dafb
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2024-07-23 19:39:44 +0300

    Changed logging symbol

--- categorizingFunctions.py
+++ categorizingFunctions.py
@@ -6,7 +6,7 @@ from time import time_ns, ctime
 from config import *
 
 log_file = open('log_file.txt', 'w')
-log_file.write('::::::::::' + str(time_ns()) + '  ||  ' + ctime() + '\n')
+log_file.write('<<<<<' + str(time_ns()) + '  ===  ' + ctime() + '\n')
 
 
 def prep_dst_dir():
@@ -70,10 +70,10 @@ def paste_file(file_path, new_dist):
         tmp_file.write(org_file.read())
         tmp_file.close()
         org_file.close()
-        log_file.write(':COPIED:' + file_path + ':' + new_dist + ':\n')
+        log_file.write('|COPIED|' + file_path + '|' + new_dist + '|\n')
     else:
         os.rename(file_path, new_dist)
-        log_file.write(':MOVED:' + file_path + ':' + new_dist + ':\n')
+        log_file.write('|MOVED|' + file_path + '|' + new_dist + '|\n')
 
 
 def progress_bar(completed):
