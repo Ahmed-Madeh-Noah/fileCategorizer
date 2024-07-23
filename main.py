9d725e09acc482216b5769777b60cb4c0dc1fbdb
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2024-07-23 18:40:34 +0300

    renaming files

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from time import time_ns
 
-import filesManipulation as fM
+import categorizingFunctions as cF
 
 if __name__ == '__main__':
-    fM.prep_dst_dir()
-    filesPath = fM.get_files_iter()
-    totalStorage = fM.enough_storage(filesPath)
+    cF.prep_dst_dir()
+    filesPath = cF.get_files_iter()
+    totalStorage = cF.enough_storage(filesPath)
     doneStorage = 0
     lastPrint = time_ns()
     for index, filePath in enumerate(filesPath):
-        fileName, fileType = fM.extract_file_attr(filePath)
-        typePath = fM.prep_type_dir(fileType)
-        newDist = fM.generate_path_name(filePath, typePath, fileName, fileType, index)
-        fM.paste_file(filePath, newDist)
-        doneStorage += fM.os.path.getsize(filePath)
+        fileName, fileType = cF.extract_file_attr(filePath)
+        typePath = cF.prep_type_dir(fileType)
+        newDist = cF.generate_path_name(filePath, typePath, fileName, fileType, index)
+        cF.paste_file(filePath, newDist)
+        doneStorage += cF.os.path.getsize(filePath)
         currentTime = time_ns()
-        if currentTime - lastPrint >= fM.UPDATING_DURATION:
+        if currentTime - lastPrint >= cF.UPDATING_DURATION:
             lastPrint = currentTime
             completed = round(doneStorage * 100 / totalStorage, 1)
             print(completed, '% of the process is completed')
-            fM.progress_bar(completed)
+            cF.progress_bar(completed)
     print('Process Completed Successfully')
