10e175c1935b04ed5b66b19cc51537abe5d6e9f3
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2024-07-25 23:36:14 +0300

    Default Window

--- UserInterface.py
+++ UserInterface.py
@@ -1,5 +1,10 @@
 import customtkinter as ctk
 
 if __name__ == '__main__':
-    root = ctk.CTk()
-    root.mainloop()
+    ctk.set_appearance_mode('System')
+    ctk.set_default_color_theme('green')
+    app = ctk.CTk()
+    app.geometry('500x500')
+    app.iconbitmap('green_folder.ico')
+    app.title('File Categorizer User Interface')
+    app.mainloop()
