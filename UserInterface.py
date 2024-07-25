e73244e4d8f4fcb5af26282985e3f962d18acac2
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2024-07-25 23:46:41 +0300

    UI Button

--- UserInterface.py
+++ UserInterface.py
@@ -1,10 +1,17 @@
 import customtkinter as ctk
 
+
+def hello():
+    print("Hello, World!!")
+
+
 if __name__ == '__main__':
-    ctk.set_appearance_mode('System')
+    ctk.set_appearance_mode('dark')
     ctk.set_default_color_theme('green')
     app = ctk.CTk()
     app.geometry('500x500')
     app.iconbitmap('green_folder.ico')
     app.title('File Categorizer User Interface')
+    button = ctk.CTkButton(app, command=hello)
+    button.pack(pady=80)
     app.mainloop()
